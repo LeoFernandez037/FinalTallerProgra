f7b024a4f6b52760374348a7267e9582c21bb442
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-06-20 11:51:20 -0400

    componente Header

---
+++ FinalTallerProgra/src/components/Ejercicios.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ejercicios = () => {
+  return <div className="tarjetaEjercicios"></div>;
+};
+
+export default Ejercicios;
---
+++ FinalTallerProgra/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="contenedorTitulo">
+      <h4>Exercicios</h4>
+      <p>4</p>
+    </div>
+  );
+};
+
+export default Header;
