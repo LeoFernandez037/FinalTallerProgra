b404a52afd4d0ed2c9cf9f21b72e27f1a62ff14c
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-06-20 11:59:55 -0400

    componente Ejercicios  acabado

--- FinalTallerProgra/src/components/Ejercicios.jsx
+++ FinalTallerProgra/src/components/Ejercicios.jsx
@@ -3,16 +3,48 @@ import "./Ejercicios.css";
 
 const Ejercicios = () => {
   return (
-    <div className="tarjetaEjercicios">
-      <img src="../public/imagen1.png" alt="no hay" />
-      <div className="description">
-        <h4>Puxada frontal</h4>
-        <p>3 series x 12 repeticoes</p>
-      </div>
-      <button className="botonVector">
+    <>
+      <div className="tarjetaEjercicios">
         <img src="../public/imagen1.png" alt="no hay" />
-      </button>
-    </div>
+        <div className="description">
+          <h4>Puxada frontal</h4>
+          <p>3 series x 12 repeticoes</p>
+        </div>
+        <button className="botonVector">
+          <img src="../public/Icons.png" alt="no hay" />
+        </button>
+      </div>
+      <div className="tarjetaEjercicios">
+        <img src="../public/imagen2.png" alt="no hay" />
+        <div className="description">
+          <h4>Remada curvada</h4>
+          <p>3 series x 12 repeticoes</p>
+        </div>
+        <button className="botonVector">
+          <img src="../public/Icons.png" alt="no hay" />
+        </button>
+      </div>
+      <div className="tarjetaEjercicios">
+        <img src="../public/imagen3.png" alt="no hay" />
+        <div className="description">
+          <h4>Remada unilateral</h4>
+          <p>3 series x 12 repeticoes</p>
+        </div>
+        <button className="botonVector">
+          <img src="../public/Icons.png" alt="no hay" />
+        </button>
+      </div>
+      <div className="tarjetaEjercicios">
+        <img src="../public/imagen4.png" alt="no hay" />
+        <div className="description">
+          <h4>Levantamiento terra</h4>
+          <p>3 series x 12 repeticoes</p>
+        </div>
+        <button className="botonVector">
+          <img src="../public/Icons.png" alt="no hay" />
+        </button>
+      </div>
+    </>
   );
 };
 
